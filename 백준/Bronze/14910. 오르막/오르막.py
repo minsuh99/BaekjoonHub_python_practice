49717b442fed4afa6ce57d7a1417520f2137f3cc
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2024-10-18 01:39:17 +0900

    [Bronze II] Title: 오르막, Time: 380 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Bronze/14910. 오르막/오르막.py
+++ 백준/Bronze/14910. 오르막/오르막.py
@@ -1,8 +1,9 @@
-def descend(ls1:list, ls2:list):
-    if ls1 == ls2:
-        return "Good"
-    return "Bad"
-
 my_list = [int(i) for i in input().split()]
-my_list2 = sorted(my_list)
-print(descend(my_list, my_list2))
+res = "Good"
+
+for i in range(len(my_list)-1):
+    if my_list[i] > my_list[i+1]:
+        res = "Bad"
+        break
+
+print(res)
