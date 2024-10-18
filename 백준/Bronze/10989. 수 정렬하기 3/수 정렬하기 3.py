344f55ee321bcf0a69a32decc8869bf1d7698a41
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2024-10-18 22:47:32 +0900

    Update 수 정렬하기 3.py

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -10,3 +10,8 @@ for i in range(len(num_list)):
     if num_list[i] != 0:
         for j in range(num_list[i]):
             print(i)
+
+
+# 정렬 시 최댓값만큼의 counting list를 만든 후
+# 인덱스가 해당 숫자, 리스트 요소 값은 해당 숫자가 나온 횟수
+# 근데도 7828ms..
