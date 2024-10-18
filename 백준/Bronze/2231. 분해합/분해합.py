94279feaa151ee1acefd73837ca8b790a38ec874
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2024-10-18 18:35:28 +0900

    [Bronze II] Title: 분해합, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,6 +1,7 @@
 n = int(input())
 res = 0
-for i in range(1, n+1):
+
+for i in range(max(1, n-9*len(str(n))), n+1):
     decompose_sum = i + sum(map(int, str(i)))
     if decompose_sum == n:
         res = i
