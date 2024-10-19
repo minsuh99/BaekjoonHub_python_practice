d496c19dfb599c16d46125a981d8a490b5a5dc4e
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2024-10-20 03:47:25 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 304 ms, Memory: 45756 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,14 @@
+import sys
+
+cor_list=[]
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    cor_list.append((a,b))
+
+cor_list.sort(key=lambda x:x[0])
+cor_list.sort(key=lambda x:x[1])
+
+for i in range(N):
+    print(cor_list[i][0], cor_list[i][1])
