4ec58321a21f8410fc5a756b2a947cbb65bd81b9
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2024-10-22 15:49:50 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,21 @@
+import sys
+
+
+while True:
+    sentence = sys.stdin.readline().rstrip()
+    if sentence == ".":
+        break
+    check_string = ""
+    for s in sentence:
+        if s not in '()[]':
+            continue
+        else:
+            check_string += s
+    for _ in range(len(check_string)):
+        check_string = check_string.replace('()', '')
+        check_string = check_string.replace('[]', '')
+
+    if len(check_string) > 0:
+        print("no")
+    else:
+        print("yes")
