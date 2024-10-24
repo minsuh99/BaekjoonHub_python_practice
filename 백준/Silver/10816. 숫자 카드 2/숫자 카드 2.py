5a32f4017684c0315d828f2d8eb07e14a6a63af0
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2024-10-25 03:56:37 +0900

    Update 숫자 카드 2.py

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -16,3 +16,7 @@ for i in card_list:
 
 for num in num_list:
     print(res_dict[num] if num in res_dict else 0, end = " ")
+
+# 딕셔너리가 시간 줄이는데 좀 도움이 되나봄
+# 리스트의 count 메소드는 O(n)이기에 리스트 요소마다 반복문을 실행하면
+# O(n^2)이라 시간 오래 걸리나봄
