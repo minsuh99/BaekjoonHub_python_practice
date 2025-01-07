973ab259fe099423a8d9c933e55f52c46fc6dae7
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-01-08 00:35:39 +0900

    [level 2] Title: 짝지어 제거하기, Time: 507.24 ms, Memory: 12.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = -1
+    stack = []
+    
+    for char in s:
+        if not stack or char not in stack:
+            stack.append(char)
+        else:
+            if char == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(char)
+    if len(stack) == 0:
+        answer = 1
+    else:
+        answer = 0
+                
+
+    return answer
