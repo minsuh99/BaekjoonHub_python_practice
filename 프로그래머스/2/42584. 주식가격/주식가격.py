f7d5b5b432bd30dd47f1c972dd086fc30a52af7b
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-01-08 01:13:25 +0900

    [level 2] Title: 주식가격, Time: 194.37 ms, Memory: 19.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,10 @@
+def solution(prices):
+    answer = [0 for _ in range(len(prices))]
+    
+    for i in range(len(prices) - 1):
+        for j in range(i + 1, len(prices)):
+            answer[i] += 1
+            if prices[j] < prices[i]:
+                break
+        
+    return answer
