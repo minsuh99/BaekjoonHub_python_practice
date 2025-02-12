cfed4d71ab9e7cba0ced2a701931c41ca2182ba0
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-02-13 07:16:37 +0900

    [level 3] Title: 네트워크, Time: 0.64 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,34 @@
+# Union-Find 스스로 짜보기
+def find(parents, x):
+    if parents[x] == x:
+        return x
+    parents[x] = find(parents, parents[x])
+    return parents[x]
+
+def union(parents, x, y):
+    x, y = min(x, y), max(x, y)
+    root1 = find(parents, x)
+    root2 = find(parents, y)
+    
+    parents[root2] = root1
+
+def solution(n, computers):
+    answer = 0
+    
+    computers_edge = []
+    for i in range(len(computers)):
+        for j in range(i, len(computers[0])):
+            if i == j:
+                continue
+            if computers[i][j] == 1:
+                computers_edge.append([i, j])
+                
+    parents = [i for i in range(n)]
+    
+    for edge in computers_edge:
+        if find(parents, edge[0]) == find(parents, edge[1]):
+            continue
+        else:
+            union(parents, edge[0], edge[1])
+    
+    return len(set(list([find(parents, i) for i in range(n)])))
