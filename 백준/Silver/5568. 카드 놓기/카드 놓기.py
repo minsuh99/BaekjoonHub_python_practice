cef1b350603cf7d3c87fbe607f1043d8510aa72d
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-02-26 01:37:30 +0900

    [Silver IV] Title: 카드 놓기, Time: 40 ms, Memory: 32924 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -0,0 +1,23 @@
+n = int(input())
+k = int(input())
+my_list = []
+res = set()
+visited = [False for _ in range(n)]
+for _ in range(n):
+    my_list.append(int(input()))
+
+def backtrack(comb_list):
+    if len(comb_list) == k:
+        temp = int("".join(map(str, comb_list[:])))
+        res.add(temp)
+        return
+    
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            comb_list.append(my_list[i])
+            backtrack(comb_list)
+            comb_list.pop()
+            visited[i] = False
+backtrack([])
+print(len(res))
