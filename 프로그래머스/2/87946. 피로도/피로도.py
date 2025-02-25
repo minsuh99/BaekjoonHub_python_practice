201aa0574024f29419891ac9aaad34218055cbc0
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-02-25 22:10:39 +0900

    [level 2] Title: 피로도, Time: 42.53 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,22 +1,17 @@
-from itertools import permutations
-
-
 def solution(k, dungeons):
     answer = -1
-    for permutation in list(permutations(dungeons, len(dungeons))):
-        cur_k = k
-        count = 0
+    visited = [False for _ in range(len(dungeons))]
+    
+    def backtrack(cur_k, count):
+        nonlocal answer
+        answer = max(answer, count)
         
-        for i in range(len(permutation)):
-            if cur_k >= permutation[i][0] and cur_k - permutation[i][1] >= 0:
-                cur_k -= permutation[i][1]
-                count += 1
-            else:
-                break
-        
-        if count == len(dungeons):
-            return len(dungeons)
-        
-        if count > answer:
-            answer = count
+        for i in range(len(dungeons)):
+            if not visited[i]:
+                min_k, minus_k = dungeons[i]
+                if cur_k >= min_k:
+                    visited[i] = True
+                    backtrack(cur_k - minus_k, count + 1)
+                    visited[i] = False
+    backtrack(k, 0)
     return answer
