c34f7d63ba61e96e5ea5565d993e6839a209039e
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-02-26 01:50:53 +0900

    [Silver III] Title: 크면서 작은 수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.py
@@ -0,0 +1,24 @@
+x = int(input())
+my_list = [int(i) for i in str(x)]
+visited = [False for _ in range(len(my_list))]
+res = 9999999
+
+def backtrack(comb_list):
+    global res
+    if len(comb_list) == len(my_list):
+        temp = int("".join(map(str, comb_list[:])))
+        if temp > x:
+            res = min(res, temp)
+        return
+    
+    for i in range(len(my_list)):
+        if not visited[i]:
+            visited[i] = True
+            comb_list.append(my_list[i])
+            backtrack(comb_list)
+            comb_list.pop()
+            visited[i] = False
+
+backtrack([])
+
+print(res if res != 9999999 else 0)
