af9fa5f6e8b257814648b95ae190ed76f3e9d7e1
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-04-22 01:49:36 +0900

    [Silver II] Title: 좌표 압축, Time: 2016 ms, Memory: 159320 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+my_list = [int(i) for i in sys.stdin.readline().split()]
+sorted_list = sorted(set(my_list))
+my_dict = dict()
+
+for i, num in enumerate(sorted_list):
+    my_dict[num] = i
+
+my_list = [my_dict[num] for num in my_list]
+
+for num in my_list:
+    print(num, end=" ")
