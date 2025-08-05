291d14a6b5706328b8d6793057859ccce02f0255
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-08-05 19:49:27 +0900

    [Silver IV] Title: 가로수, Time: 4564 ms, Memory: 41928 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,27 @@
+def gcd(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        if a < b:
+            big, small = b, a
+        else:
+            big, small = a, b
+    
+        return gcd(big%small, small)
+
+N = int(input())
+my_list = []
+for _ in range(N):
+    my_list.append(int(input()))
+
+diff_list = [my_list[i + 1] - my_list[i] for i in range(N - 1)]
+final_gcd = 0
+
+final_gcd = gcd(diff_list[0], diff_list[1])
+if len(diff_list) != 2:
+    for num in diff_list[2:]:
+        final_gcd = gcd(final_gcd, num)
+
+print(len(range(my_list[0], my_list[-1] + 1, final_gcd)) - len(my_list))
