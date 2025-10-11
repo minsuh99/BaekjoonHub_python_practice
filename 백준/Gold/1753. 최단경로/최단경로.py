157e7829ed172f5acad21b1e9487491b805351f7
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-10-11 11:43:54 +0900

    [Gold IV] Title: 최단경로, Time: 816 ms, Memory: 74644 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,20 +1,20 @@
-from queue import PriorityQueue
 import sys
+from queue import PriorityQueue
 input = sys.stdin.readline
 
 V, E = map(int, input().split())
 K = int(input())
-distance = [float('inf')] * (V + 1)
-visited = [False] * (V + 1)
-my_list = [[] for _ in range(V + 1)]
+graph = [[] for _ in range(V + 1)]
+visited = [False for _ in range(V + 1)]
+dist = [float('inf') for _ in range(V + 1)]
 queue = PriorityQueue()
 
 for _ in range(E):
     u, v, w = map(int, input().split())
-    my_list[u].append((v, w))
+    graph[u].append((v, w))
 
 queue.put((0, K))
-distance[K] = 0
+dist[K] = 0
 
 while queue.qsize() > 0:
     cur = queue.get()
@@ -23,15 +23,11 @@ while queue.qsize() > 0:
         continue
     visited[cur_v] = True
     
-    for temp in my_list[cur_v]:
-        next_v, next_w = temp[0], temp[1]
-        if distance[next_v] > distance[cur_v] + next_w:
-            distance[next_v] = distance[cur_v] + next_w
-            queue.put((distance[next_v], next_v))
-            
+    for temp in graph[cur_v]:
+        next_node, weight = temp[0], temp[1]
+        if dist[next_node] > dist[cur_v] + weight:
+            dist[next_node] = dist[cur_v] + weight
+            queue.put((dist[next_node], next_node))
 
-for i in range(1, V + 1):
-    if visited[i]:
-        print(distance[i])
-    else:
-        print("INF")
+for d in dist[1:]:
+    print(d if d != float('inf') else "INF")
