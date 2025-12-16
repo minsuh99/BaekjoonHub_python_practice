7c5dfaa126e05736372f9a851b19469785e9e9b8
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 01:22:14 +0900

    [Bronze V] Title: A+B - 8, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,8 +1,5 @@
-import sys
-
-T = int(sys.stdin.readline())
+T = int(input())
 
 for i in range(T):
-    num_list =[int(i) for i in sys.stdin.readline().split()]
-    res = sum(num_list)
-    print(f"Case #{i+1}: {num_list[0]} + {num_list[1]} = {res}")
+    a, b = map(int, input().split())
+    print(f"Case #{i+1}: {a} + {b} = {a+b}")
