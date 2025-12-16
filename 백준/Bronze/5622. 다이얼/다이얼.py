c1a2919dc320ed584f06da9bce8af310252c97b6
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 02:22:56 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,24 +1,14 @@
-def dial(c):
-    if c in ['A', 'B', 'C']:
-        return 3
-    elif c in ['D','E','F']:
-        return 4
-    elif c in ['G','H','I']:
-        return 5
-    elif c in ['J','K','L']:
-        return 6
-    elif c in ['M','N','O']:
-        return 7
-    elif c in ['P','Q','R', 'S']:
-        return 8
-    elif c in ['T','U','V']:
-        return 9
-    elif c in ['W','X','Y','Z']:
-        return 10
+dial = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"], 
+        ["J", "K", "L"], ["M", "N", "O"], ["P", "Q", "R", "S"],
+        ["T", "U", "V"], ["W", "X", "Y", "Z"]]
 
 word = input()
 res = 0
-for alphabet in word:
-    res += dial(alphabet)
+
+for w in word:
+    for i, lis in enumerate(dial):
+        if w in lis:
+            res += 3 + i
+            break
 
 print(res)
