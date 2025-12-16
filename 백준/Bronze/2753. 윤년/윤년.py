2ca3f928e0c54e534c5454097ae90cef653e3450
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 00:43:30 +0900

    [Bronze V] Title: 윤년, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,6 @@
-year = int(input())
+y = int(input())
 
-if ((year % 4 == 0) & (year % 100 != 0) | (year % 400 == 0)):
+if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
     print(1)
 else:
     print(0)
