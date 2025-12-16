115abd19e98a5fdbd48ecb61e58e256c19ae23fb
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 02:30:08 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,6 +1,5 @@
-chess = [1, 1, 2, 2, 2, 8]
+og_list = [1, 1, 2, 2, 2, 8]
+now = list(map(int, input().split()))
 
-my_list = [int(i) for i in input().split()]
-
-for i in range(len(chess)):
-    print(chess[i]-my_list[i], end=" ")
+for i in range(6):
+    print(og_list[i] - now[i], end=" ")
