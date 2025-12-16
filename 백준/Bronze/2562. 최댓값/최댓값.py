f2bd99bf4d4c617861f07b38d00d8df238f28ded
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 01:41:33 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,10 +1,11 @@
-res = 0
-idx = 0
-for i in range(1,10):
-    num = int(input())
-    if num > res:
-        res = num
-        idx = i
+max_val = 0
+max_idx = 0
 
-print(res)
-print(idx)
+for i in range(9):
+    val = int(input())
+    if val > max_val:
+        max_val = val
+        max_idx = i + 1
+
+print(max_val)
+print(max_idx)
