c715110b9a3da62f57dde399343da4f2b1cd23d1
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 00:38:23 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,13 +1,12 @@
 score = int(input())
 
-match score // 10:
-    case 10 | 9:
-        print('A')
-    case 8:
-        print('B')
-    case 7:
-        print('C')
-    case 6:
-        print('D')
-    case _:
-        print('F')
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
