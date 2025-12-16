224664bc76b979711f16501c155ed1b2e4273933
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 01:01:42 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,12 +1,14 @@
-def alarm(h, m, c):
-    m += c
-    if m >= 60:
-        h += (m // 60)
-        m %= 60
-        if h >= 24:
-            h %= 24
-    print(h, m)
-    
-h, m = map(int, input().split())
+a, b = map(int, input().split())
 c = int(input())
-alarm(h, m, c)
+
+a += (c//60)
+b += (c%60)
+
+if b >= 60:
+    b -= 60
+    a += 1
+    
+if a >= 24:
+    a %= 24
+
+print(a, b)
