5474ef5c075d99dca4e62e35cb81b5d6991976a6
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 03:23:26 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,15 +1,11 @@
-my_mat = [[0 for _ in range(101)] for _ in range(101)]
-res = 0
-n = int(input())
+my_list = [[0 for _ in range(100)] for _ in range(100)]
+N = int(input())
 
-for _ in range(n):
+for _ in range(N):
     x, y = map(int, input().split())
     
-    for i in range(x, x+10):
-        for j in range(y, y+10):
-            my_mat[i][j] = 1
+    for i in range(10):
+        for j in range(10):
+            my_list[y + i][x + j] = 1
 
-for i in range(100):
-    res += sum(my_mat[i])
-
-print(res)
+print(sum([sum(l) for l in my_list]))
