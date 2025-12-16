bc381d10ec10ec387c3fe4857e22b486054667ad
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 01:06:53 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,10 +1,12 @@
 a, b, c = map(int, input().split())
 
-if a == b and b == c:
-    print(10000+a*1000)
-elif a == b or a == c:
-    print(1000 + a * 100)
-elif b == c:
-    print(1000 + b * 100)
+temp1 = [a, b, c]
+temp2 = set(temp1)
+
+if len(temp1) == len(temp2):
+    print(max(temp1) * 100)
 else:
-    print(max(a,b,c)*100)
+    if len(temp1) - len(temp2) == 1:
+        print(1000 + (sum(temp1) - sum(temp2)) * 100)
+    else:
+        print(10000 + temp1[0] * 1000)
