0d1fd4ed68434960529f36286dc18857c7639490
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 00:56:10 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,11 +1,12 @@
-def alarm(h, m):
-    if m - 45 < 0:
-        m += 60
-        if h == 0:
-            h = 24
-        h -= 1
-    m -= 45
-    print(h, m)
-    
 h, m = map(int, input().split())
-alarm(h, m)
+
+m -= 45
+
+if m < 0:
+    m += 60
+    h -= 1
+    
+    if h < 0:
+        h += 24
+
+print(h, m)
