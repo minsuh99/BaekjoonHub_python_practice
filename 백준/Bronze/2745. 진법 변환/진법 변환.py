912e101588257d64a9992e95495dd7bd8e3ccbb7
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-17 03:31:57 +0900

    [Bronze II] Title: 진법 변환, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,12 +1,2 @@
-input_list = input().split()
-num = input_list[0][::-1]
-b = int(input_list[1])
-res = 0
-
-for i in range(len(num)):
-    if num[i].isdigit():
-        res += int(num[i]) * (b ** i)
-    elif num[i].isalpha():
-        res += (ord(num[i]) - ord('A') + 10) * (b ** i)
-
-print(res)
+N, B = map(str, input().split())
+print(int(N, base=int(B)))
