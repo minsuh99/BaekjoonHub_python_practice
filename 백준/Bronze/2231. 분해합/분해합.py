73d45fe64eb6a9836402432bf6e4040ecd577a1f
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-19 02:12:34 +0900

    [Bronze II] Title: 분해합, Time: 1116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,41 +1,13 @@
-# def decompose_sum(n: int):
-#     return n + sum([int(i) for i in str(n)])
-    
+import sys
+input = sys.stdin.readline
+N = int(input())
 
-# n = int(input())
-# res = 0
-# for i in range(n):
-#     if decompose_sum(i) == n:
-#         res = i
-#         break
-# print(res)
-
-# 브루트포스 알고리즘
-# 1176ms
-
-# n = int(input())
-# res = 0
-# for i in range(1, n+1):
-#     decompose_sum = i + sum(map(int, str(i)))
-#     if decompose_sum == n:
-#         res = i
-#         break
-
-# print(res)
-
-# 1104 ms
-
-n = int(input())
+min_search = int(10**(len(str(N)) - 2))*9
 res = 0
 
-for i in range(max(1, n-9*len(str(n))), n):
-    decompose_sum = i + sum(map(int, str(i)))
-    if decompose_sum == n:
-        res = i
+for num in range(min_search, N):
+    temp = [int(i) for i in str(num)]
+    if num + sum(temp) == N:
+        res = num
         break
-
 print(res)
-
-# 32ms
-# 탐색 시작점을 잘 찾으면 다 탐색할 필요 없다..
-# 3자리 정수이면 3자리 정수의 자릿수 합 최댓값을 빼면 시작점이 될것이다..
