33ab6946a9f698292766d105d24df5a1b325f58b
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-19 02:25:27 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,13 +1,9 @@
-import math
-
 a, b, c, d, e, f = map(int, input().split())
 
 if b != 0:
-    x = (b*f- c*e) // (b*d - a*e)
-    y = (c - a * x) // b
-
+    x = (f - c*e/b) / (d - a*e/b)
+    y = (c - a*x) / b
 else:
-    x = c // a
-    y = (f - d * x) // e
-
-print(x, y)
+    x = c / a
+    y = (f - d*x) / e
+print(round(x), round(y))
