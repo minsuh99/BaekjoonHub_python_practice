ef0903dce57cf4be253a94cee2aa88b97bb28efc
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-18 23:55:19 +0900

    [Bronze III] Title: 대지, Time: 1292 ms, Memory: 35916 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,14 +1,9 @@
-n = int(input())
-if n == 1:
-    a, b = map(int, input().split())
-    print(0)
-else:
-    max_x, max_y, min_x, min_y = -10000, -10000, 10000, 10000
-    for _ in range(n):
-        a, b = map(int, input().split())
-        max_x = max(a, max_x)
-        max_y = max(b, max_y)
-        min_x = min(a, min_x)
-        min_y = min(b, min_y)
+N = int(input())
+coord_x, coord_y = [], []
 
-    print((max_x - min_x) * (max_y - min_y))
+for _ in range(N):
+    x, y = map(int, input().split())
+    coord_x.append(x)
+    coord_y.append(y)
+
+print((max(coord_x) - min(coord_x)) * (max(coord_y) - min(coord_y)))
