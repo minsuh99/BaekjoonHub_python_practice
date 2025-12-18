0dcc0b1f0e970debd766fab3ae44a07eb9718d8e
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-19 03:04:21 +0900

    [Silver V] Title: 영화감독 숌, Time: 728 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,36 +1,11 @@
-# disaster_num = []
-# num = 666
-# while len(disaster_num) <= 10000:
-#     if str(num).find('666') != -1:
-#         disaster_num.append(num)
-#     num += 1
-# print(disaster_num[int(input())-1])
-
-# 1192 ms
-
-disaster_num = []
-num = 666
 N = int(input())
-while len(disaster_num) < N:
-    if str(num).find('666') != -1:
-        disaster_num.append(num)
-    num += 1
-print(disaster_num[-1])
-
-# 1184ms
-# while 때문인듯
-
-N = int(input())
-count = 0
-num = 666
+check_num = 666
+cnt = 0
 
 while True:
-    if '666' in str(num):
-        count += 1
-    if count == N:
-        print(num)
-        break
-    num += 1
-    
-# 584ms
-# 리스트도 한 몫 하는듯
+    if '666' in str(check_num):
+        cnt += 1
+    if cnt == N:
+        print(check_num)
+        exit()
+    check_num += 1
