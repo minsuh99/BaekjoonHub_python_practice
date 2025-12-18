98f617270d232b36118b8ce98fb02c52abe96e32
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-18 16:45:04 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,15 +1,14 @@
-def is_prime(n:int):
-    cnt = 0
-    for i in range(1, n+1):
-        if n % i == 0:
-            cnt += 1
-    if cnt == 2:
-        return True
-    else:
+def is_prime(n):
+    if n == 1:
         return False
+    
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
 
-
-
-n = int(input())
+N = int(input())
 num_list = [int(i) for i in input().split()]
 print(sum([is_prime(i) for i in num_list]))
+
