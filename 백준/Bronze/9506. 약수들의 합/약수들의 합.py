f42a69b7108a7c79252d9e02c92648acda40e14d
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-18 16:40:16 +0900

    [Bronze I] Title: 약수들의 합, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,16 +1,16 @@
 while True:
-    n = int(input())
-    if n == -1:
-        break
-    fac_list = []
+    N = int(input())
+    divisors = []
+    if N == -1:
+        exit()
     
-    for i in range(1, int(n ** 0.5)+1):
-        if n % i == 0:
-            fac_list.append(i)
-            fac_list.append(n // i)
-    fac_list = sorted(fac_list)[:-1]
-    if n == sum(fac_list):
-        fac_list = [str(i) for i in fac_list]
-        print(f'{n} = {" + ".join(fac_list)}')
+    for i in range(1, N):
+        if N % i == 0:
+            divisors.append(i)
+    
+    if sum(divisors) == N:
+        res = str(N) + " = " + " + ".join([str(i) for i in divisors])
+        
+        print(res)
     else:
-        print(f'{n} is NOT perfect.')
+        print(f"{N} is NOT perfect.")
