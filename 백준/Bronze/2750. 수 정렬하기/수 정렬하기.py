3af93b7dfa54fde5b7e446aaac4a4ff08366cd50
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-19 04:00:06 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,11 +1,16 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-my_list=[]
-for _ in range(n):
-    my_list.append(int(sys.stdin.readline()))
+N = int(input())
+my_list = []
 
-my_list.sort()
+for _ in range(N):
+    my_list.append(int(input()))
+
+for i in range(len(my_list) - 1):
+    for j in range(i + 1, len(my_list)):
+        if my_list[i] > my_list[j]:
+            my_list[i], my_list[j] = my_list[j], my_list[i]
 
 for num in my_list:
     print(num)
