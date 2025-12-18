38acaa482d8df2b293971304aa58fc575ab3e208
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-19 01:06:52 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
@@ -2,7 +2,10 @@ a1, a0 = map(int, input().split())
 c = int(input())
 n0 = int(input())
 
-if (a1*n0+a0)<=c*n0 and a1<=c:
-    print(1)
+if a1 * n0 + a0 <= c * n0:
+    if c >= a1:
+        print(1)
+    else:
+        print(0)
 else:
     print(0)
