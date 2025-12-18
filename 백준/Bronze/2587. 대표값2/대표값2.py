f048def83fb219b1d85b8c413ebf5245a6e9aa02
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-19 04:03:29 +0900

    [Bronze II] Title: 대표값2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,6 +1,15 @@
+import sys
+input = sys.stdin.readline
+
 my_list = []
+
 for _ in range(5):
     my_list.append(int(input()))
 
+for i in range(len(my_list) - 1):
+    for j in range(i + 1, len(my_list)):
+        if my_list[i] > my_list[j]:
+            my_list[i], my_list[j] = my_list[j], my_list[i]
+
 print(sum(my_list) // len(my_list))
-print(sorted(my_list)[2])
+print(my_list[2])
