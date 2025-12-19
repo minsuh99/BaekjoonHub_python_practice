97478dd4d40db5fff5e9b281dfed07f8d39f2187
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-19 16:24:45 +0900

    [Bronze II] Title: 커트라인, Time: 152 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.py
+++ 백준/Bronze/25305. 커트라인/커트라인.py
@@ -1,6 +1,12 @@
-n, k = map(int, input().split())
-my_list = [int(i) for i in input().split()]
+import sys
+input = sys.stdin.readline
 
-my_list.sort(reverse=True)
+N, k = map(int, input().split())
+score = [int(i) for i in input().split()]
 
-print(my_list[k-1])
+for i in range(N - 1):
+    for j in range(i, N):
+        if score[i] < score[j]:
+            score[i], score[j] = score[j], score[i]
+
+print(score[k-1])
