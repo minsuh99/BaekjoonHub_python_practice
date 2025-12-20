a33c2123ad6fc6bf2937575ee0ce53ca138e1ff9
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-21 02:34:37 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7776 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,17 +1,12 @@
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-num_list = [0] * 10001
+N = int(input())
+my_list = [0 for _ in range(10001)]
+
 for _ in range(N):
-    num = int(sys.stdin.readline())
-    num_list[num] += 1
+    my_list[int(input())] += 1
 
-for i in range(len(num_list)):
-    if num_list[i] != 0:
-        for j in range(num_list[i]):
-            print(i)
-
-
-# 정렬 시 최댓값만큼의 counting list를 만든 후
-# 인덱스가 해당 숫자, 리스트 요소 값은 해당 숫자가 나온 횟수
-# 근데도 7828ms..
+for i in range(10001):
+    for j in range(my_list[i]):
+        print(i)
