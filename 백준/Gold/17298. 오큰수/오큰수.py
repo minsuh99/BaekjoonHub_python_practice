5f36071a7d6525271be757239b8f0eded8bebbe3
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-24 00:39:29 +0900

    [Gold IV] Title: 오큰수, Time: 1212 ms, Memory: 202288 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,16 +1,18 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
 my_list = [int(i) for i in input().split()]
 res_list = [-1 for _ in range(N)]
+
 stack = []
 
 for i, num in enumerate(my_list):
-    while stack and my_list[stack[-1][0]] < my_list[i]:
-        idx, _ = stack.pop()
-        res_list[idx] = my_list[i]
+    if stack:
+        while True:
+            if stack and stack[-1][1] < num:
+                idx, _ = stack.pop()
+                res_list[idx] = num
+            else:
+                break
+
     stack.append((i, num))
 
-for num in res_list:
-    print(num, end=" ")
+print(*res_list)
