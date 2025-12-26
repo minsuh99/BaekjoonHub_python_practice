ace2342b5d1652d89223f95a0803ee19695a752e
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-26 15:09:03 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,11 +1,11 @@
 def gcd(a, b):
-    res = 0
-    for i in range(1, min(a,b)+1):
-        if a % i == 0 and b % i == 0:
-            res = i
-    return res
+    if a == 0:
+        return b
+    else:
+        x, y = max(a, b), min(a, b)
+        return gcd(x % y, y)
 
-
-a, b = map(int, input().split())
-print(gcd(a,b))
-print(a*b//gcd(a,b))
+A, B = map(int, input().split())
+G = gcd(A, B)
+print(G)
+print(A*B//G)
