eecba3788bfa3e8ba037cc6c325271175ca44f67
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-29 14:11:46 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,11 +1,9 @@
+dp = [0 for _ in range(1001)]
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, 1001):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
 n = int(input())
-
-my_list = [0 for _ in range(1001)]
-
-my_list[0] = 1
-my_list[1] = 1
-
-for i in range(2, 1001):
-    my_list[i] = my_list[i - 2] + my_list[i - 1]
-
-print(my_list[n] % 10007)
+print(dp[n] % 10007)
