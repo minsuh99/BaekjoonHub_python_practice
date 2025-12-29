871322697a9887066305c78047c90e09cfe7f19c
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-29 13:58:21 +0900

    [Silver III] Title: 1로 만들기, Time: 800 ms, Memory: 40660 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,11 @@
-Max = 1000001
-d = [0 for _ in range(Max)]
+n = int(input())
+d = [0 for _ in range(n + 1)]
 d[1] = 0
-d[2] = 1
-d[3] = 1
 
-for i in range(4, Max):
+for i in range(2, n + 1):
     flag1 = (i % 2 != 0)
     flag2 = (i % 3 != 0)
-    d[i] = min(d[i-1] + 1, (flag1 * Max + (d[int(i // 2)] + 1)), (flag2 * Max + (d[int(i // 3)] + 1)))
+    d[i] = min(d[i-1] + 1, (flag1 * n + (d[int(i // 2)] + 1)), (flag2 * n + (d[int(i // 3)] + 1)))
 
-n = int(input())
+
 print(d[n])
