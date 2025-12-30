0480aa262b2ef7e2054f4086c919ce2b53b51e9c
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-30 12:13:49 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+rgb = [[0, 0, 0]]
+for _ in range(N):
+    rgb.append(list(map(int, input().split())))
+
+dp = [[0 for _ in range(3)] for _ in range(N + 1)]
+
+for i in range(N + 1):
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2]
+
+print(min(dp[N]))
