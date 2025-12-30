bde496b7fc62a22472364a0d71612b3f5ffa001a
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2025-12-30 14:04:52 +0900

    [Silver I] Title: 스티커, Time: 788 ms, Memory: 48116 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    score = [[] for _ in range(2)]
+    for i in range(2):
+        score[i] = list(map(int, input().split()))
+            
+    dp = [[0 for _ in range(n)] for _ in range(2)]
+    dp[0][0] = score[0][0]
+    dp[1][0] = score[1][0]
+    
+    if n >= 2:
+        dp[0][1] = score[0][1] + dp[1][0]
+        dp[1][1] = score[1][1] + dp[0][0]
+    
+        for i in range(2, n):
+            dp[0][i] = max(dp[0][i - 2], dp[1][i - 2], dp[1][i - 1]) + score[0][i]
+            dp[1][i] = max(dp[0][i - 2], dp[1][i - 2], dp[0][i - 1]) + score[1][i]
+
+    print(max(dp[0][n - 1], dp[1][n - 1]))
