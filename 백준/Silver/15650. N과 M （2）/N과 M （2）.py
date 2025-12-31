8d449216176f7c020ded65a6dbcf7160f9c34845
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-01 05:33:24 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,4 +1,7 @@
+import sys
+sys.setrecursionlimit(10**6)
 from itertools import combinations
-n, m = map(int, input().split())
-for comp in list(combinations(range(1, n + 1), m)):
-    print(" ".join(map(str, comp)))
+N, M = map(int, input().split())
+my_list = [int(i) for i in range(1, N + 1)]
+for res in list(combinations(my_list, M)):
+    print(*res)
