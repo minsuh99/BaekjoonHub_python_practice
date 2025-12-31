237a53ece74ea7f429859ddf898f3b724a552c15
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-01 06:29:48 +0900

    [Silver III] Title: N과 M (3), Time: 1864 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -3,6 +3,18 @@ sys.setrecursionlimit(10 ** 9)
 from itertools import product
 
 N, M = map(int, input().split())
-my_list = [int(i) for i in range(1, N + 1)]
-for res in list(product(my_list, repeat=M)):
-    print(*res)
+# my_list = [int(i) for i in range(1, N + 1)]
+# for res in list(product(my_list, repeat=M)):
+#     print(*res)
+
+def backtrack(res_list):
+    if len(res_list) == M:
+        print(*res_list)
+        return
+    
+    for i in range(N):
+        res_list.append(i + 1)
+        backtrack(res_list)
+        res_list.pop()
+
+backtrack([])
