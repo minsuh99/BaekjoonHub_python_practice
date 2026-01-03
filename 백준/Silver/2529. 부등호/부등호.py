08c6c5488cf93b13e8880577716357cd2de1b95b
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-03 23:51:14 +0900

    [Silver I] Title: 부등호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -1,37 +1,65 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10 ** 7)
 
 k = int(input())
-inequal = input().rstrip().split()
+inequal = input().split()
 
-res_max = ""
+res_max = None
 res_min = None
-visited = [False for _ in range(10)]
+
+visited_max = [False for _ in range(10)]
+visited_min = [False for _ in range(10)]
 
 def valid(a, b, op):
-    return a < b if op == '<' else a > b
+    if op == '<':
+        return a < b
+    else:
+        return a > b
 
-def backtrack(res):
-    global res_max, res_min
 
-    if len(res) == k + 1:
-        s = "".join(map(str, res))
-        if res_min is None or s < res_min:
-            res_min = s
-        if s > res_max:
-            res_max = s
-        return
+def backtrack_max(res_list):
+    global res_max
 
-    for d in range(10):
-        if visited[d]:
+    if len(res_list) == k + 1:
+        res_max = "".join(map(str, res_list))
+        return True
+
+    for cur_num in range(9, -1, -1):
+        if visited_max[cur_num]:
             continue
-        if len(res) == 0 or valid(res[-1], d, inequal[len(res)-1]):
-            visited[d] = True
-            backtrack(res + [d])
-            visited[d] = False
 
-backtrack([])
+        if len(res_list) == 0 or valid(res_list[-1], cur_num, inequal[len(res_list) - 1]):
+            visited_max[cur_num] = True
+            if backtrack_max(res_list + [cur_num]):
+                return True
+            visited_max[cur_num] = False
+
+    return False
+
+
+def backtrack_min(res_list):
+    global res_min
+
+    if len(res_list) == k + 1:
+        res_min = "".join(map(str, res_list))
+        return True
+
+    for cur_num in range(10):
+        if visited_min[cur_num]:
+            continue
+
+        if len(res_list) == 0 or valid(res_list[-1], cur_num, inequal[len(res_list) - 1]):
+            visited_min[cur_num] = True
+            if backtrack_min(res_list + [cur_num]):
+                return True
+            visited_min[cur_num] = False
+
+    return False
+
+
+backtrack_max([])
+backtrack_min([])
 
 print(res_max)
 print(res_min)
