ea1c6ead4be8d1182c0a518c9da132e825f02860
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-07 15:11:49 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32504 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,5 +1,6 @@
-from collections import deque
+# from collections import deque
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 N = int(input())
@@ -12,25 +13,36 @@ for i in range(N):
     board[i] = list(map(int, input().rstrip()))
 
 
-def bfs(cur_r, cur_c):
-    visited[cur_r][cur_c] = True
-    queue = deque([(cur_r, cur_c)])
-    area = 1
+# def bfs(cur_r, cur_c):
+#     visited[cur_r][cur_c] = True
+#     queue = deque([(cur_r, cur_c)])
+#     area = 1
     
-    while queue:
-        r, c = queue.popleft()
-        for dr, dc in drdc:
-            if 0 <= r + dr < N and 0 <= c + dc < N:
-                if visited[r + dr][c + dc] == False and board[r + dr][c + dc] == 1:
-                    visited[r + dr][c + dc] = True
-                    queue.append((r + dr, c + dc))
-                    area += 1
+#     while queue:
+#         r, c = queue.popleft()
+#         for dr, dc in drdc:
+#             if 0 <= r + dr < N and 0 <= c + dc < N:
+#                 if visited[r + dr][c + dc] == False and board[r + dr][c + dc] == 1:
+#                     visited[r + dr][c + dc] = True
+#                     queue.append((r + dr, c + dc))
+#                     area += 1
+#     return area
+
+def dfs(cur_r, cur_c):
+    visited[cur_r][cur_c] = True
+    area = 1
+    for dr, dc in drdc:
+        if 0 <= cur_r + dr < N and 0 <= cur_c + dc < N:
+            if not visited[cur_r + dr][cur_c + dc] and board[cur_r + dr][cur_c + dc] == 1:
+                area += dfs(cur_r + dr, cur_c + dc)
     return area
+
 
 for r in range(N):
     for c in range(N):
         if visited[r][c] == False and board[r][c] == 1:
-            res.append(bfs(r, c))
+            # res.append(bfs(r, c))
+            res.append(dfs(r, c))
 
 res.sort()
 print(len(res))
