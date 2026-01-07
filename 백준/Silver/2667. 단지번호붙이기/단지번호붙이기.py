e03e35f9f79ea12ed2cda3cb5c577bb6c473e65e
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-07 14:53:51 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34992 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+board = [[] for _ in range(N)]
+visited = [[False for _ in range(N)] for _ in range(N)]
+drdc = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+res = []
+
+for i in range(N):
+    board[i] = list(map(int, input().rstrip()))
+
+
+def bfs(cur_r, cur_c):
+    visited[cur_r][cur_c] = True
+    queue = deque([(cur_r, cur_c)])
+    area = 1
+    
+    while queue:
+        r, c = queue.popleft()
+        for dr, dc in drdc:
+            if 0 <= r + dr < N and 0 <= c + dc < N:
+                if visited[r + dr][c + dc] == False and board[r + dr][c + dc] == 1:
+                    visited[r + dr][c + dc] = True
+                    queue.append((r + dr, c + dc))
+                    area += 1
+    return area
+
+for r in range(N):
+    for c in range(N):
+        if visited[r][c] == False and board[r][c] == 1:
+            res.append(bfs(r, c))
+
+res.sort()
+print(len(res))
+for ans in res:
+    print(ans)
