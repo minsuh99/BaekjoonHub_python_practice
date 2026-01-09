bd189fccbcfdfb1c5bbe42451c9386df0b979048
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-10 01:22:06 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 55960 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,33 +1,11 @@
-import sys
-
-N = int(sys.stdin.readline())
-num_list = [int(i) for i in range(1, N+1)]
-
-while len(num_list) > 1:
-    if len(num_list) % 2 == 0:
-        num_list = num_list[1::2]
-    else:
-        num_list = num_list[1::2]
-        temp = num_list.pop(0)
-        num_list.insert(len(num_list), temp)
-
-print(num_list[0])
-
-# 84ms
-# 너무 수학적임
-
 from collections import deque
+N = int(input())
+num_list = deque([int(i) for i in range(1, N + 1)])
 
-N = int(sys.stdin.readline())
-card = deque([i for i in range(1, N+1)])
-
-while len(card) > 1:
-    card.popleft()
-    card.rotate(-1)
-
-print(card[0])
-
-# 180ms
-# 오히려 이게 더 메모리를 잡아먹고 시간도 좀 걸리는듯.. 왜지
-# popleft: 가장 왼쪽의 요소를 pop
-# rotate: 왼쪽으로 한 칸 회전 2 3 4 -> 3 4 2
+while True:
+    if len(num_list) == 1:
+        print(num_list[0])
+        exit()
+    
+    num_list.popleft()
+    num_list.append(num_list.popleft())
