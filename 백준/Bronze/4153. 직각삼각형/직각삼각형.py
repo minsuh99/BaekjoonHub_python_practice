146d7a8bcba8374fe1b67847f137e9e7a0b650a5
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-09 23:12:32 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,11 +1,20 @@
 import sys
+input = sys.stdin.readline
 
 while True:
-    a, b, c = map(int, sys.stdin.readline().split())
-    if a==0 & b==0 & c==0:
-        break
-    num_list = sorted([a, b, c])
-    if num_list[0]**2 + num_list[1]**2 == num_list[2]**2:
+    a, b, c = map(int, input().split())
+    if a == b == c == 0:
+        exit()
+    max_side = max([a, b, c])
+    left = 0
+    right = 0
+    for side in [a, b, c]:
+        if side != max_side:
+            left += side ** 2
+        else:
+            right += side ** 2
+    
+    if left == right:
         print("right")
     else:
         print("wrong")
