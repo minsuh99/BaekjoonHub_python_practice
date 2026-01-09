855ed902de47c618147972dc9d0cf53cd7631aed
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-10 01:08:04 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,15 +1,19 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-my_list = []
-rank = [1] * N
-for _ in range(N):
-    x, y = map(int, input().split())
-    my_list.append((x,y))
+rank_list = [1 for _ in range(N)]
+people = []
 
-for i in range(len(my_list)):
-    for j in range(len(my_list)):
-        if i==j: continue
-        elif (my_list[i][0] < my_list[j][0]) & (my_list[i][1] < my_list[j][1]):
-            rank[i] += 1
-
-for i in range(len(rank)):
-    print(rank[i], end=" ")
+for i in range(N):
+    weight, height = map(int, input().split())
+    people.append((weight, height))
+    if i == 0:
+        continue
+    for j in range(i):
+        if weight > people[j][0] and height > people[j][1]:
+            rank_list[j] += 1
+        elif weight < people[j][0] and height < people[j][1]:
+            rank_list[i] += 1
+        
+print(*rank_list)
