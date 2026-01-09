2c14803b45335093708cd0154eedff1c42e690da
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-10 01:17:30 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 52272 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,12 +1,10 @@
-import sys
+N = int(input())
+num_list = set([int(i) for i in input().split()])
+M = int(input())
+check_num = [int(i) for i in input().split()]
 
-n = int(sys.stdin.readline())
-n_list = set([int(i) for i in sys.stdin.readline().split()]) # set으로 저장하면 탐색 시간이 줄어듬 / list와의 차이
-m = int(sys.stdin.readline())
-m_list = [int(i) for i in sys.stdin.readline().split()]
-
-for i in range(m):
-    print(1) if m_list[i] in n_list else print(0)
-
-# 시간 초과,,
-# set vs list
+for num in check_num:
+    if num in num_list:
+        print(1)
+    else:
+        print(0)
