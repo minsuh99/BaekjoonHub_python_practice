b079eff120cb5f07b515c49cd0fe62db92082b4e
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-10 00:14:30 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,11 +1,6 @@
-fac_list = []
+fac = [1 for _ in range(11)]
+for i in range(1, 11):
+    fac[i] = fac[i - 1] * i
 
-for i in range(11):
-    if i == 0:
-        fac_list.append(1)
-    else:
-        fac_list.append(fac_list[i-1] * i)
-
-n, k = map(int, input().split())
-
-print(fac_list[n] // (fac_list[k] * fac_list[n-k]))
+N, K = map(int, input().split())
+print(fac[N] // (fac[K] * fac[N - K]))
