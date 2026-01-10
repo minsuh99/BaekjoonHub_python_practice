d8c1d3ab734be17543db1f6a656c81a01b85b550
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-11 02:19:40 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,21 +1,24 @@
+import sys
+
 N = int(input())
-T = int(input())
+graph = [[] for _ in range(N + 1)]
+edges = int(input())
 
-computers = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+for _ in range(edges):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
 visited = [False for _ in range(N + 1)]
-
-for _ in range(T):
-    a, b = map(int, input().split())
-    computers[a][b] = 1
-    computers[b][a] = 1
-
-def dfs(graph, node, visited):
+res = []
+def dfs(node):
+    global res
     visited[node] = True
-    for i in range(len(graph[node])):
-        if graph[node][i] == 1:
-            if visited[i] == False:
-                visited[i] = True
-                dfs(graph, i, visited)
+    
+    for next_node in graph[node]:
+        if not visited[next_node]:
+            res.append(next_node)
+            dfs(next_node)
 
-dfs(computers, 1, visited)
-print(sum(visited) - 1)
+dfs(1)
+print(len(res))
