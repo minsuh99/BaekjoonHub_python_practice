aaa1b1af6c1a6282726abf51ef7de03b2673edb3
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-10 19:06:41 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,16 +1,18 @@
 import sys
+input = sys.stdin.readline
 
-n, k = map(int, sys.stdin.readline().split())
-coin_list = []
-
-for _ in range(n):
-    coin_list.append(int(sys.stdin.readline()))
-
+N, K = map(int, input().split())
+coin = []
 res = 0
-for money in coin_list[::-1]:
-    if k // money == 0:
-        continue
-    res += (k // money)
-    k %= money
+
+for _ in range(N):
+    coin.append(int(input()))
+
+for i in range(N - 1, -1, -1):
+    res += K // coin[i]
+    K %= coin[i]
+    
+    if K == 0:
+        break
 
 print(res)
