9fef266d1d9cc3c5e8c084b4c0c981f99296c322
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-01-13 11:08:41 +0900

    [Silver II] Title: 랜선 자르기, Time: 88 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,19 +1,36 @@
-k, n= map(int, input().split())   
-l = [int(input()) for _ in range(k)]
+import sys
+input = sys.stdin.readline
+# 만들려는 랜선의 길이가 커질 수록
+# 그 랜선의 개수는 작아지고,
+# 반대로 랜선 길이가 작아질 수록 랜선 개수가 많아짐
+# 일관성 + 단조성이 있어 이분 탐색이 적절(하다는 생각을 떠올려야함)
 
-start = 1   
-end = max(l)   
+K, N = map(int, input().split())
+lan = []
+for _ in range(K):
+    lan.append(int(input()))
 
-while start <= end:   
-    mid = (start + end) // 2   
-    res = 0   
+lan.sort()
 
-    for l_ in l:
-        res += l_ // mid
-    
-    if res >= n:   
-        start = mid + 1   
-    else:   
-        end = mid - 1   
+left = 1
+right = max(lan)
+mid = (left + right) // 2
 
-print(end)
+while True:
+    if left > right:
+        break
+    res = 0
+    for l in lan:
+        res += (l // mid)
+    # 현재 랜선의 개수가 필요한 것보다 더 많기 때문에
+    # mid 길이가 필요한 최대 길이보다 짧다는 것
+    # left를 mid + 1로 이동
+    if res >= N:
+        left = mid + 1
+    # 반대는 right를 mid - 1로 이동
+    elif res < N:
+        right = mid - 1
+        
+    mid = (left + right) // 2
+
+print(right)
