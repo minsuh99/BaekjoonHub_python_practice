2381929d05226e5a7a8937afa6745721613e514c
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-20 03:09:45 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,18 +1,9 @@
 def solution(n):
     answer = 0
-    tri = ''
-    max_p = 0
-    while True:
-        if n < 3 ** max_p:
-            break
-        max_p += 1
-    max_p -= 1
-    
-    while max_p >= 0:
-        tri += str(n // (3 ** max_p))
-        n -= int(tri[-1]) * 3 ** max_p
-        max_p -= 1
-    
-    for i, num in enumerate(tri):
-        answer += int(num) * 3 ** i
+    tri = []
+    while n > 0:
+        tri.append(n % 3)
+        n //= 3
+    for i, t in enumerate(tri[::-1]):
+        answer += t * (3 ** i)
     return answer
