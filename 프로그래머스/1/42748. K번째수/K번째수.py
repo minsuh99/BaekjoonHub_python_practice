a187331e327e23c877fa5c4451ef2d44b032c83f
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-20 04:09:31 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,5 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        i, j, k = map(int, command)
-        check_array = sorted(array[i-1:j])
-        answer.append(check_array[k-1])
-        
+        answer.append(sorted(array[command[0] - 1:command[1]])[command[2] - 1])
     return answer
