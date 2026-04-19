ef150ca43c9e5f1c7dabf4cd6f2a92cc538808be
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-20 02:33:26 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,13 +1,11 @@
 def solution(d, budget):
     answer = 0
-    d = sorted(d)
-    # '최대' 몇 개의 부서에게 지원이 가능한지 묻는 거니까
-    # 작은 예산부터 예산을 주고 다음 부서에게 줄 남은 금액이 있으면
-    # 그 다음 예산의 부서에게 부여하는게 타당함.
-    
-    for money in d:
-        if money <= budget:
-            answer += 1
-            budget -= money
-    
+    d.sort()
+    price = 0
+    for p in d:
+        if price + p > budget:
+            break
+        price += p
+        answer += 1
+        
     return answer
