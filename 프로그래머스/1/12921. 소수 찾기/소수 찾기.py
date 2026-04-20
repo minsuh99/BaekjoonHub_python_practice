916060b696fac4ef5409bad348fdca97eda471bd
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-20 18:49:39 +0900

    [level 1] Title: 소수 찾기, Time: 129.84 ms, Memory: 22.6 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,10 +1,9 @@
 def solution(n):
-    answer = 0
-    num_of_div = [0 for _ in range(n + 1)]
+    is_prime = [False, False] + [True for _ in range(n - 1)]
     
-    for i in range(1, n + 1):
-        for j in range(i, n + 1, i):
-            num_of_div[j] += 1
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
     
-    answer = len([k for k in num_of_div if k == 2])
-    return answer
+    return sum(is_prime)
