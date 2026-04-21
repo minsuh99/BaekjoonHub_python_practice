c601ed65399253edcababa13fc16563cafc3d8fe
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-22 03:36:25 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 9.1 MB, 시간: 0.04 ms
+메모리: 9.15 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 10일 01:37:03
+2026년 04월 22일 03:36:22
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,27 +1,12 @@
 def solution(n, lost, reserve):
-    answer = 0
-    lost.sort()
-    reserve.sort()
+    overlap = set(lost) & set(reserve)
+    lost = set(lost) - overlap
+    reserve = set(reserve) - overlap
 
-    clothes = [1 for _ in range(n + 2)]
-    
-    for student in lost:
-        clothes[student] -= 1
-    
-    for student in reserve:
-        clothes[student] += 1
-    
-    borrow_student = [s for s in lost if s not in reserve]
-    for student in borrow_student:
-        if clothes[student - 1] == 2:
-            clothes[student - 1] -= 1
-            clothes[student] += 1
-            continue
-        elif clothes[student + 1] == 2:
-            clothes[student + 1] -= 1
-            clothes[student] += 1
-            continue
-    
-    print(clothes[1:-1])
-    answer = n - len([i for i in clothes if i == 0])
-    return answer
+    for r in sorted(reserve):
+        if r - 1 in lost:
+            lost.remove(r - 1)
+        elif r + 1 in lost:
+            lost.remove(r + 1)
+
+    return n - len(lost)
