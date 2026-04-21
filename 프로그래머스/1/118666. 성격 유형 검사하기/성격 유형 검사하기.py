0caa45d60b6118eccd7c4cc489031a6f86f998ce
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-22 04:51:30 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.45 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,22 +1,24 @@
 def solution(survey, choices):
     answer = ''
     index_ = ["RT", "CF", "JM", "AN"]
-    index_score = [{k:0 for k in idx} for idx in index_]
-    
-    for i, choice in enumerate(choices):
+    index_score = [{k: 0 for k in idx} for idx in index_]
+
+    for pair, choice in zip(survey, choices):
         if choice == 4:
             continue
-        
-        if survey[i] in index_:
-            idx = index_.index(survey[i])
+
+        for i, idx in enumerate(index_):
+            if pair[0] in idx and pair[1] in idx:
+                if choice < 4:
+                    index_score[i][pair[0]] += 4 - choice
+                else:
+                    index_score[i][pair[1]] += choice - 4
+                break
+
+    for pair, d in zip(index_, index_score):
+        if d[pair[0]] >= d[pair[1]]:
+            answer += pair[0]
         else:
-            idx = index_.index(survey[i][::-1])
-            
-        if choice > 4:
-            index_score[idx][survey[i][1]] += abs(choice - 4)
-        
-        elif choice < 4:
-            index_score[idx][survey[i][0]] += abs(choice - 4)
-    
-    answer = "".join([max(d, key=d.get) for d in index_score])
+            answer += pair[1]
+
     return answer
