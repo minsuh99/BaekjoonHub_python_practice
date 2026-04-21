16301bae71bbc5e150aae053c3d8886aa94a1b83
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-22 03:38:40 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,12 +1,26 @@
 def solution(n, lost, reserve):
-    overlap = set(lost) & set(reserve)
-    lost = set(lost) - overlap
-    reserve = set(reserve) - overlap
-
-    for r in sorted(reserve):
-        if r - 1 in lost:
-            lost.remove(r - 1)
-        elif r + 1 in lost:
-            lost.remove(r + 1)
-
-    return n - len(lost)
+    answer = 0
+    have_cloth = [True if i + 1 not in lost else False for i in range(n)]
+    reserve_cloth = [True if i + 1 in reserve else False for i in range(n)]
+    
+    for i in range(n):
+        if not have_cloth[i] and reserve_cloth[i]:
+            have_cloth[i] = True
+            reserve_cloth[i] = False
+    
+    for i in range(n):
+        if not have_cloth[i]:
+            if reserve_cloth[i]:
+                have_cloth[i] = True
+                reserve_cloth[i] = False
+                continue
+            elif i > 0 and reserve_cloth[i - 1]:
+                have_cloth[i] = True
+                reserve_cloth[i - 1] = False
+                continue
+            elif i < n - 1 and reserve_cloth[i + 1]:
+                have_cloth[i] = True
+                reserve_cloth[i + 1] = False
+                continue
+    
+    return sum(have_cloth)
