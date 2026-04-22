debbff08b9d07b457c3559b263473fcf194dc2ee
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-22 23:05:10 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.43 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,16 +1,15 @@
 def solution(s):
     answer = []
-    ans1 = 0
-    ans2 = 0
-    temp = 0
+    cnt_zero = 0
+    cnt = 0
     
     while s != "1":
-        s_remove0 = "".join([i for i in s if i != '0'])
-        temp = len(s_remove0)
-        ans2 += (len(s) - temp)
-        s = str(bin(temp))[2:]
-        ans1 += 1
-    answer = [ans1, ans2]
-
-
+        cnt += 1
+        new_s = s.replace("0", "")
+        cnt_zero += len(s) - len(new_s)
+        s = bin(len(new_s))[2:]
+        
+    answer = [cnt, cnt_zero]
+    
     return answer
+
