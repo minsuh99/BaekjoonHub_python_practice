357b0bf7c1575f3e6226efcc63f3481efa48429e
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-23 00:47:37 +0900

    [level 2] Title: 피보나치 수, Time: 425.78 ms, Memory: 438 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,11 @@
 def solution(n):
-    fibo_num = [0 for _ in range(100001)]
-    fibo_num[1] = 1
-    for i in range(2, n+1):
-        fibo_num[i] = fibo_num[i-2] + fibo_num[i-1]
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
     
-    answer = (fibo_num[n] % 1234567)
-    
-    return answer
+    fibo = [0, 1] + [0 for _ in range(2, n + 1)]
+    for i in range(2, n + 1):
+        fibo[i] = fibo[i - 1] + fibo[i - 2]
+
+    return fibo[n] % 1234567
