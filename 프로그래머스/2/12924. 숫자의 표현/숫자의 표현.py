2f6f0424f8fcd2f44f2b8c1351a12c697e94fabf
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-23 00:39:04 +0900

    [level 2] Title: 숫자의 표현, Time: 1.94 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,12 +1,15 @@
 def solution(n):
     answer = 0
     start, end = 1, 1
+    partial_sum = 1
     while end <= n:
-        partial_sum = sum(range(start, end + 1))
         if partial_sum <= n:
             if partial_sum == n:
                 answer += 1
             end += 1
+            partial_sum += end
         else:
+            partial_sum -= start
             start += 1
+            
     return answer
