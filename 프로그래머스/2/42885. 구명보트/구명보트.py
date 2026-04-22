08f13578a3c2e774c13077897173c6a996b8399a
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-23 02:21:46 +0900

    [level 2] Title: 구명보트, Time: 10.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,19 +1,21 @@
+from collections import deque
+
+
 def solution(people, limit):
     answer = 0
-    people = sorted(people)
-    boat = 0
+    people = deque(sorted(people))
+    cur_w = 0
     
-    left_idx = 0
-    right_idx = len(people) - 1
-    
-    while left_idx <= right_idx:
-        boat += people[right_idx]
-        right_idx -= 1
-        
-        if people[left_idx] <= limit - boat:
-            left_idx += 1
+    while people:
+        if cur_w == 0:
+            cur_w += people.pop()
+        else:
+            if people[0] + cur_w <= limit:
+                cur_w = 0
+                people.popleft()
+            else:
+                cur_w = people.pop()
+            answer += 1
+    if cur_w > 0:
         answer += 1
-        
-        boat = 0
-
     return answer
