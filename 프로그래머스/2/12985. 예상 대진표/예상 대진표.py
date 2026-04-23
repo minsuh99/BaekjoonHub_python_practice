bf65ba421c46906ccda8c76e9d5979c54e4cca70
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-24 03:34:14 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,15 +1,9 @@
-def solution(n,a,b):
+def solution(n, a, b):
     answer = 0
-
+    
     while a != b:
-        if a % 2 != 0:
-            a += 1
-        a //= 2
-        
-        if b % 2 != 0:
-            b += 1
-        b //= 2
-        
+        a = (a + 1) // 2
+        b = (b + 1) // 2
         answer += 1
-
+    
     return answer
