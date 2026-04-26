c37bdb703784201e55e95201153c5a5d0c5aa5e7
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-27 04:22:31 +0900

    [level 2] Title: 튜플, Time: 27.42 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,11 +1,15 @@
 def solution(s):
     answer = []
-    my_list = s[1:-1].split("},{")
-    my_list = [c.replace("{", "").replace("}", "") for c in my_list]
-    my_list = [set([int(i) for i in l.split(",")]) for l in my_list]
-    my_list.sort(key=lambda x:len(x))
-    
-    for st in my_list:
-        for element in st - set(answer):
-            answer.append(element)
+    seen = set()
+
+    my_list = s[2:-2].split("},{")
+    my_list = [list(map(int, x.split(","))) for x in my_list]
+    my_list.sort(key=len)
+
+    for group in my_list:
+        for num in group:
+            if num not in seen:
+                answer.append(num)
+                seen.add(num)
+
     return answer
