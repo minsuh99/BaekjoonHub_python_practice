f5a7c09ff416620664a744df4506e227e83bb866
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-27 04:37:09 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.03 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,41 +1,29 @@
 from collections import Counter
 
+
 def solution(str1, str2):
-    answer = 0
+    str1, str2 = str1.lower(), str2.lower()
+    my_list1, my_list2 = [], []
     
-    str1 = str1.lower()
-    str2 = str2.lower()
+    for i in range(len(str1) - 1):
+        if str1[i].isalpha() and str1[i+1].isalpha():
+            my_list1.append(str1[i]+str1[i+1])
     
-    arr1 = [str1[i:i+2] for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
-    arr2 = [str2[i:i+2] for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+    for i in range(len(str2) - 1):
+        if str2[i].isalpha() and str2[i+1].isalpha():
+            my_list2.append(str2[i]+str2[i+1])
     
+    my_dict1, my_dict2 = Counter(my_list1), Counter(my_list2)
+    all_keys = list(set(my_dict1.keys()) | set(my_dict2.keys()))
+    if len(my_dict1) == 0 and len(my_dict2) == 0:
+        return 65536
     
-    set1 = dict(Counter(arr1))
-    set2 = dict(Counter(arr2))
+    inter_dict, union_dict = dict(), dict()
+    for k in all_keys:
+        if k in my_dict1 and k in my_dict2:
+            inter_dict[k] = min(my_dict1[k], my_dict2[k])
+        union_dict[k] = max(my_dict1.get(k, -1), my_dict2.get(k, -1))
     
-    intersection = dict()
-    union = dict()
+    jaccard = int(sum(inter_dict.values()) / sum(union_dict.values()) * 65536)
     
-    for key in set1.keys():
-        if key in set2:
-            intersection[key] = min(set1[key], set2[key])
-            union[key] = max(set1[key], set2[key])
-
-    while True:
-        for key in set1.keys():
-            union[key] = set1[key]
-        
-        for key in set2.keys():
-            if key in intersection.keys():
-                union[key] = max(set1[key], set2[key])
-            else:
-                union[key] = set2[key]
-        
-        break
-    
-    if sum(union.values()) == 0:
-        answer = 65536
-    else:
-        answer = int(sum(intersection.values()) / sum(union.values()) * 65536)
-
-    return answer
+    return jaccard
