5a04415a1b82e220bea514c3d9af62a443f8413d
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-27 05:01:08 +0900

    [level 3] Title: 정수 삼각형, Time: 52.73 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,15 +1,14 @@
 def solution(triangle):
-    # 위 -> 아래 방향은 중간중간 경우의 수가 많음
-    # 아래 -> 위 방향으로 계산,, (어쨌든 결과는 맨 아래 숫자를 만나야 끝나니까)
-    # 바로 위로 올라갈때, 왼쪽과 오른쪽 중 더 큰 숫자를 위로 올라가면서 더하면 됨.
-    # 참고함
+    dp = [[0 for _ in range(i)] for i in range(1, len(triangle) + 1)]
+    dp[0][0] = triangle[0][0]
     
-    my_list = [[0] * i for i in range(1, len(triangle)+1)]
-    my_list[-1] = triangle[-1]
-    
-    for i in range(len(my_list) - 2, -1, -1):
-        for j in range(len(triangle[i])):
-            my_list[i][j] += triangle[i][j] + max(my_list[i+1][j], my_list[i+1][j+1])
-    
-    return my_list[0][0]
-    
+    for r in range(1, len(triangle)):
+        for c in range(r + 1):
+            if c == 0:
+                dp[r][c] = dp[r - 1][c] + triangle[r][c]
+            elif c == r:
+                dp[r][c] = dp[r - 1][c - 1] + triangle[r][c]
+            else:
+                dp[r][c] = max(dp[r - 1][c - 1], dp[r - 1][c]) + triangle[r][c]
+
+    return max(dp[-1])
