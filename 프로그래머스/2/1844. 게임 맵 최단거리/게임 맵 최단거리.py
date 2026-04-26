6148001ac887d22dc6c69446732aa841b78dbb84
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-27 03:51:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.25 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,31 @@
 from collections import deque
-# 혼자 짜보기 (bfs 이용 최단거리 찾기)
-# def bfs(start_x, start_y, end_x, end_y, maps): 
-# 기존에 이렇게 짰는데, [목표지점] end_x, end_y가 maps의 오른쪽 하단점으로 고정이라 없어도 될 듯
-def bfs(start_x, start_y, maps):
-    n = len(maps)
-    m = len(maps[0])
-    dxdy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    visited = set([(start_x, start_y)])
-    queue = deque([(start_x, start_y, 1)])
-    
-    while queue:
-        x, y, dist = queue.popleft()
-        if x == n -1 and y == m - 1:
-            return dist
-        for d in dxdy:
-            x_, y_ = x + d[0], y + d[1]
-            if 0 <= x_ < n and 0 <= y_ < m:
-                if (x_, y_) not in visited:
-                    if maps[x_][y_] == 1:
-                        visited.add((x_, y_))
-                        queue.append((x_, y_, dist + 1))
-    return -1
-    
-    
+
 
 def solution(maps):
-    answer = bfs(0, 0, maps)
-    return answer
+    answer = -1
+    n, m = len(maps), len(maps[0])
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    drdc = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    
+    
+    def bfs():
+        nonlocal n, m
+        nonlocal visited
+        nonlocal drdc
+        visited[0][0] = True
+        queue = deque([(0, 0, 1)])
+        
+        while queue:
+            r, c, cnt = queue.popleft()
+            if r == n - 1 and c == m - 1:
+                return cnt
+            for dr, dc in drdc:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < m:
+                    if not visited[nr][nc] and maps[nr][nc] == 1:
+                        visited[nr][nc] = True
+                        queue.append((nr, nc, cnt + 1))
+        
+        return -1
+    
+    return bfs()
