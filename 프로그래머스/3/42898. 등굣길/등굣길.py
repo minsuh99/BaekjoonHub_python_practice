131d58c1a5e3d708f159fcd5a4fec837650c9154
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-27 19:57:23 +0900

    [level 3] Title: 등굣길, Time: 4.59 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,17 @@
+def solution(m, n, puddles):
+    answer = 0
+    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    dp[1][1] = 1
+
+    for r in range(1, n + 1):
+        for c in range(1, m + 1):
+            if [c, r] in puddles:
+                dp[r][c] = 0
+                continue
+            else:
+                if r == 1 and c == 1:
+                    continue
+                else:
+                    dp[r][c] = (dp[r - 1][c] + dp[r][c - 1]) % 1000000007
+    
+    return dp[-1][-1] % 1000000007
