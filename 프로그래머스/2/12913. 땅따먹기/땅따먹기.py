02b92a2ec691ed79827be478e79cf2766dcab950
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-04-28 20:18:01 +0900

    [level 2] Title: 땅따먹기, Time: 403.45 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,8 +1,10 @@
 def solution(land):
-    my_list = [[0 for _ in range(4)] for _ in range(len(land))]
-    my_list[-1] = land[-1]
+    answer = 0
+    dp = [[0 for _ in range(len(land[0]))] for _ in range(len(land))]
+    dp[0] = land[0].copy()
     
-    for i in range(len(my_list) - 2, -1, -1):
-        for j in range(len(my_list[i])):
-            my_list[i][j] = land[i][j] + max([my_list[i+1][k] for k in range(len(my_list[i+1])) if k != j])
-    return max(my_list[0])
+    for r in range(1, len(dp)):
+        for c in range(len(dp[0])):
+            dp[r][c] = max([value for i, value in enumerate(dp[r - 1]) if i != c]) + land[r][c]
+        
+    return max(dp[-1])
