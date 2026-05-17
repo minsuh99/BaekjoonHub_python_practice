e93188a49f988da9d33dd9cdb20a68b89891e8c8
Minsuh99 <90837545+minsuh99@users.noreply.github.com>
2026-05-17 23:45:28 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,20 +1,22 @@
+from collections import defaultdict
+
 def solution(genres, plays):
     answer = []
-    genres_play_dict = {}
+
+    genre_total = defaultdict(int)
+    genre_songs = defaultdict(list)
     
-    for i, genre in enumerate(genres):
-        if genre not in genres_play_dict:
-            genres_play_dict[genre] = plays[i]
-        else:
-            genres_play_dict[genre] += plays[i]
-    genres_play_dict = dict(sorted(genres_play_dict.items(), key = lambda x: -x[1])) 
-    # 딕셔너리 정렬 검색,,
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
+        genre_total[genre] += play
+        genre_songs[genre].append((play, idx))
     
-    for genre in list(genres_play_dict.keys()):
-        idx_list = [g[0] for g in list(enumerate(genres)) if g[1] == genre]
+    # 장르를 총 재생 수 기준 내림차순 정렬
+    sorted_genres = sorted(genre_total.keys(), key=lambda x: genre_total[x], reverse=True)
+    
+    for genre in sorted_genres:
+        songs = sorted(genre_songs[genre], key=lambda x: (-x[0], x[1]))
         
-        temp_plays = [play for play in list(enumerate(plays)) if play[0] in idx_list]
-        temp_plays = sorted(temp_plays, key = lambda x: -x[1])
-        final_idx = [i[0] for i in temp_plays[:2]]
-        answer.extend(final_idx)
+        for play, idx in songs[:2]:
+            answer.append(idx)
+    
     return answer
